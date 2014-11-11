6c32070d3e590e25f89ff6f5827ad7f79ce226c7
Abdi Dahir <abdi.dahir@outlook.com>
2014-11-10 23:09:12 -0500

    tests show that concurrency is kind of busted

---
+++ test/concurrency.js
@@ -0,0 +1,111 @@
+var assert = require('assert');
+var should = require('should');
+var mongoose = require('mongoose');
+var Model = require('../models/model.js').Model;
+
+describe('Mongoose Rollback Machine', function(done) {
+    console.log('Testing global functionailty, things that alter state everywhere.');
+    before(function(done) {
+        mongoose.connect('mongodb://localhost/test', {});
+        var db = mongoose.connection;
+        db.on('error', console.error.bind(console, 'connection err:'));
+        db.once('open', function() {
+            //console.log('Connected to Mongo DB at ' + db.host + ":" + db.port);
+        });
+
+        done();
+    });
+
+    after(function(done) {
+        mongoose.disconnect();
+        done();
+    });
+    
+    beforeEach(function(done) {
+        Model.find({}).remove(function(err, result) {
+            if (err) throw (err);
+            var history = Model.collection.name + "_hist";
+            mongoose.connection.collections[history].remove({}, function(err, result) {
+                if (err) throw (err);   
+                    done();
+            });
+        });
+    });
+
+    describe('Two Models', function(done) {
+
+        it('Save two independant models without any issue', function(done) {
+            var model = new Model({name: 'Hello', data: 'World'});
+            model.save(function(err, model) {
+                if (err) throw (err);
+                model.name.should.match('Hello');
+                model._version.should.be.exactly(0);
+
+                model.name = "Hey";
+                model.data = "Yo";
+
+                model.save(function(err, model) {
+                    if (err) throw (err);
+
+                    model.name.should.match('Hey');
+                    model._version.should.be.exactly(1);
+                });
+            });
+
+            var model2 = new Model({name: 'Hello', data: 'World'});
+            model2.save(function(err, model2) {
+               if (err) throw (err);
+               model2.name.should.match('Hello');
+               model2._version.should.be.exactly(0);
+            });
+
+            // realbad
+            setTimeout(function() {
+                done();
+            }, 200);
+                     
+        });
+
+        it('Save the same model twice', function(done) {
+
+            var model = new Model({name: 'Hello', data: 'World'});
+            model.save(function(err, model) {
+                if (err) throw (err);
+                model.name.should.match('Hello');
+                model._version.should.be.exactly(0);
+
+                model.name = "Hey";
+                model.data = "Yo";
+
+                model.save(function(err, newmodel) {
+                    if (err) throw (err);
+
+                    console.log(newmodel);
+                    console.log(err);
+                    //model.name.should.match('Hey');
+                    console.log(newmodel._version);
+                });
+
+                model.name = "Booyah";
+                model.save(function(err, newmodel) {
+                    if (err) throw (err);
+
+                    console.log(newmodel);
+                    console.log(err);
+                    //model.name.should.match('Booyah');
+                    console.log(newmodel._version);
+                });
+
+                // realbad
+                setTimeout(function() {
+                    model.history(0, 100, function(err, models) {
+                        console.log(models);
+                        console.log(err);
+                    });
+                    done();
+                }, 200);
+
+            });
+        });
+    });
+});
